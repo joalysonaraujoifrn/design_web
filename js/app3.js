55e43d2389160e21fbfbf08900af9bea757ac54e
Joalyson <joalyson.araujo@escolar.ifrn.edu.br>
2026-08-26 17:56:21 -0300

    app3

---
+++ js/app3.js
@@ -0,0 +1,15 @@
+let nasc = 2008;
+let nome = "Joalyson";
+let vivo = true;
+let altura = 1.79;
+let sexo;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`Ano de nascimento - tipo: ${typeof(nasc)}`)
+alert(`Nome - tipo: ${typeof(nome)}`)
+alert(`Vivo - tipo: ${typeof(vivo)}`)
+alert(`Altura - tipo: ${typeof(altura)}`)
+alert(`Sexo - tipo: ${typeof(sexo)}`)
+alert(`Peso - tipo: ${typeof(peso)}`)
+alert(`IMC - tipo: ${typeof(imc)}`)
