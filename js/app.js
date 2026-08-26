3712985e7506326877427a7b20e79a940d6018d9
Joalyson <joalyson.araujo@escolar.ifrn.edu.br>
2026-08-26 17:14:02 -0300

    ex var

--- js/app.js
+++ js/app.js
@@ -4,11 +4,20 @@ const viva = true;
 
 function calcIdade(ano=2026){
     let idade = ano - nasc;
-    alert(`Dentro da função - Idade ${idade}`);
+    let menor;
+    if (idade < 18){
+        menor = true;
+        var podebeber = false;
+    }else{
+        menor = false;
+        var podebeber = true;
+    }
+    alert(`${nome} é menor de idade? ${menor}\nIdade: ${idade}\nPode beber: ${podebeber}`)
     return idade;
 }
 
 calcIdade();
+alert(`Pode beber: ${podebeber}`);
 /*
 alert(`Fora da função : Idade ${idade}`);
 Erro pois a variável let não existe fora do escopo dela
